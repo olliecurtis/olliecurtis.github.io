005c7bfe3d897b4f599127c2aeb392e6850879c2
olliecurtis <olliecurtis@users.noreply.github.com>
2021-05-06 21:01:23 +0000

    deploy: e893e87527f4017682b65def9902d342a6e99449

---
+++ precache-manifest.44a3d1629adf692cbf43661d92385c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f107d39d4cd5147f12bbbad76fe41e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "696522f80394057004ad",
+    "url": "/static/css/main.7446681d.chunk.css"
+  },
+  {
+    "revision": "49cdb44d65edc2d78904",
+    "url": "/static/js/2.4d1cebe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d1cebe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696522f80394057004ad",
+    "url": "/static/js/main.371084d1.chunk.js"
+  },
+  {
+    "revision": "14451483c95e1aded9d3",
+    "url": "/static/js/runtime-main.3571a7d5.js"
+  }
+]);
